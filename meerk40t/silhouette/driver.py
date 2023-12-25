76b13f778c62266d500a922d914df1b69fea3080
tiger12506 <tiger12506@gmail.com>
2023-12-25 01:54:05 -0500

    Scaffolding the Silhouette driver (part 2)

--- meerk40t/silhouette/driver.py
+++ meerk40t/silhouette/driver.py
@@ -1,4 +1,17 @@
+"""
+Silhouette Driver
+
+Converts generic commands of LaserJob, et al. into Silhouette code
+
+Author: Tiger12506
+Heavily references: https://github.com/fablabnbg/inkscape-silhouette
+"""
+import time
 from ..core.units import UNITS_PER_INCH, UNITS_PER_MIL, UNITS_PER_MM
+from meerk40t.core.cutcode.gotocut import GotoCut
+from meerk40t.core.cutcode.homecut import HomeCut
+from meerk40t.core.cutcode.linecut import LineCut
+from meerk40t.core.cutcode.waitcut import WaitCut
 
 CMD_EOT = b'\x04'
 CMD_ENQ = b'\x05'
@@ -42,16 +55,36 @@ class SilhouetteDriver:
         self(f"job started {job}")
 
     def move_abs(self, x, y):
-        self(f"move_abs({x}, {y})")
+        self(f"M{x},{y}")
+
+    def draw_abs(self, x, y):
+        self(f"D{x},{y}")
 
     def move_rel(self, dx, dy):
         self(f"move_rel({dx}, {dy})")
 
-    def plot(self, cutobject):
-        self(f"plot send? {cutobject}")
+    def plot(self, plot):
+        self(f"plot({plot})")
+        self.queue.append(plot)
 
     def plot_start(self):
-        self("plot start!")
+        self("plot_start()")
+        for q in self.queue:
+            while self.hold_work(0):
+                if self.service.kernel.is_shutdown:
+                    return
+                time.sleep(0.05)
+            if isinstance(q, LineCut):
+                self.move_abs(*q.start)
+                self.draw_abs(*q.end)
+            elif isinstance(q, HomeCut):
+                self.home()
+            elif isinstance(q, WaitCut):
+                self.wait(q.dwell_time)
+            elif isinstance(q, GotoCut):
+                self.move_abs(*q.end)
+        self.queue.clear()
+        return False
 
     def job_finish(self, job):
         self(f"job finished {job}")
@@ -64,6 +97,9 @@ class SilhouetteDriver:
 
     def home(self):
         self("Home!")
+
+    def wait(self, dwell_time):
+        time.sleep(dwell_time)
 
     def laser_on(self):
         self("Cutter down!")
