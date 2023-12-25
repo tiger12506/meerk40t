c32a96ab0de50bdd2b0a80b1522c669d13b2a1b5
tiger12506 <tiger12506@gmail.com>
2023-12-25 00:59:33 -0500

    Scaffolding the Silhouette driver.

--- meerk40t/silhouette/driver.py
+++ meerk40t/silhouette/driver.py
@@ -1,3 +1,9 @@
+from ..core.units import UNITS_PER_INCH, UNITS_PER_MIL, UNITS_PER_MM
+
+CMD_EOT = b'\x04'
+CMD_ENQ = b'\x05'
+CMD_ETX = b'\x03'
+
 
 class SilhouetteDriver:
     def __init__(self, service, channel=None, *args, **kwargs):
@@ -9,27 +15,58 @@ class SilhouetteDriver:
         self.native_x = 0
         self.native_y = 0
 
+        self.stepper_step_size = UNITS_PER_MM * 0.05  # Characteristic of Silhouette machines, step size is 0.05mm
+        self.queue = []
+
+        self.out_pipe = print
+        self.in_pipe = input
+
     def __repr__(self):
         return f"SilhouetteDriver({self.name})"
 
     def __call__(self, e, real=False):
-        print(e)
+        if real: self.out_pipe(e)
+        else: print(e)
+
+    def initialize(self):
+        self.send_escaped(CMD_EOT)
+
+    def status(self):
+        self(CMD_ENQ)
 
     def hold_work(self, priority):
-        """
-        Required.
-
-        Spooler check. to see if the work cycle should be held.
-
-        @return: hold?
-        """
+        self(f"spooler check: {priority}")
         return False
 
     def job_start(self, job):
-        pass
+        self(f"job started {job}")
+
+    def move_abs(self, x, y):
+        self(f"move_abs({x}, {y})")
+
+    def move_rel(self, dx, dy):
+        self(f"move_rel({dx}, {dy})")
+
+    def plot(self, cutobject):
+        self(f"plot send? {cutobject}")
+
+    def plot_start(self):
+        self("plot start!")
 
     def job_finish(self, job):
-        pass
+        self(f"job finished {job}")
+
+    def pause(self):
+        self("Pause!")
+
+    def resume(self):
+        self("Resume!")
 
     def home(self):
         self("Home!")
+
+    def laser_on(self):
+        self("Cutter down!")
+
+    def laser_off(self):
+        self("Cutter up!")
